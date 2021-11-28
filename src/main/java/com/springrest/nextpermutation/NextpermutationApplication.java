a031bc0f5298ad621258e5b96139cd5348632568
Ajit Jain <ajitj@jfrog.com>
2021-11-28 18:47:21 +0530

    Project Initialized

---
+++ src/main/java/com/springrest/nextpermutation/NextpermutationApplication.java
@@ -0,0 +1,13 @@
+package com.springrest.nextpermutation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextpermutationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextpermutationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springrest/nextpermutation/NextpermutationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springrest.nextpermutation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextpermutationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
